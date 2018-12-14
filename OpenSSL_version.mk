d469f19358036348bf64a64dd7679d84d0fc9fcd
David von Oheimb <David.von.Oheimb@siemens.com>
2018-12-14 20:30:10 +0100

    correct reference to OpenSSL libs in OpenSSL_version.mk

--- OpenSSL_version.mk
+++ OpenSSL_version.mk
@@ -1,13 +1,22 @@
 ifeq ($(OS),Windows_NT)
     EXE=.exe
+    LIB=bin
 else
     EXE=
+    LIB=lib
 endif
 
 #CC=gcc
 ifneq ($(OPENSSL_DIR),)
+    ifeq ($(shell echo $(OPENSSL_DIR) | grep "^/"),)
+    # $(OPENSSL_DIR) is relative path
+        OPENSSL_LIB=$(OPENSSL_DIR)
+    else
+    # $(OPENSSL_DIR) is absolute path
+        OPENSSL_LIB=$(OPENSSL_DIR)/$(LIB)
+    endif
     CFLAGS+=-I$(OPENSSL_DIR)/include
-    LDFLAGS+=-L$(OPENSSL_DIR) -Wl,-rpath=$(OPENSSL_DIR)
+    LDFLAGS+=-L$(OPENSSL_LIB) -Wl,-rpath=$(OPENSSL_LIB)
 endif
 LDLIBS=-lcrypto
 

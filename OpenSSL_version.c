e74dac0a95599183af855865d7d2cbe4d0b4f40e
David von Oheimb <David.von.Oheimb@siemens.com>
2018-12-19 18:05:55 +0100

    make build and use of OpenSSL_version more robust

--- OpenSSL_version.c
+++ OpenSSL_version.c
@@ -5,10 +5,13 @@
 #if OPENSSL_VERSION_NUMBER < 0x10002000L
 #error Better not use OpenSSL versions older than 1.0.2. They are unsupported and insecure.
 #endif
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#define OpenSSL_version_num SSLeay
+#endif
 
 int main(int argc, char *argv[])
 {
-    unsigned long runtime_version = SSLeay(); /* or since v1.1.0: OpenSSL_version_num(); */
+    unsigned long runtime_version = OpenSSL_version_num();
     if (runtime_version != OPENSSL_VERSION_NUMBER) {
         fprintf(stderr, "OpenSSL runtime version 0x%lx does not match version 0x%lx used by compiler\n",
                 runtime_version, OPENSSL_VERSION_NUMBER);

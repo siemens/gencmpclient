31fca5e83e9d49db2c576a17dd088cd93c298819
Dr. David von Oheimb <David.von.Oheimb@siemens.com>
2022-02-23 09:48:25 +0100

    did.sh: fix EJBCA references

--- did.sh
+++ did.sh
@@ -1,5 +1,22 @@
 #! /usr/bin/bash
 
+export  EJBCA_HOST="ppki-playground.ct.siemens.com" \
+	EJBCA_OCSP_URL="http://ppki-playground.ct.siemens.com/ejbca/publicweb/status/ocsp" \
+	 EJBCA_CDP_URL_PREFIX="http://ppki-playground.ct.siemens.com/ejbca/publicweb/webdist/certdist?cmd=crl&format=DER&issuer=CN=PPKI+Playground+" \
+	EJBCA_CDPS="Infrastructure+Root+CA+v1.0 Infrastructure+Issuing+CA+v1.0 ECC+Root+CA+v1.0 RSA+Root+CA+v1.0" \
+	EJBCA_CDP_URL_POSTFIX="%2cOU=Corporate+Technology%2cOU=For+internal+test+purposes+only%2cO=Siemens%2cC=DE" \
+	EJBCA_CMP_ISSUER="creds/PPKI_Playground_ECCIssuingCAv10.crt" \
+	EJBCA_CMP_CLIENT="creds/PPKI_Playground_CMP.p12" \
+	EJBCA_TLS_CLIENT="creds/PPKI_Playground_TLS.p12" \
+	EJBCA_CMP_TRUSTED="creds/PPKI_Playground_ECCRootCAv10.crt" \
+	EJBCA_TRUSTED="creds/PPKI_Playground_InfrastructureRootCAv10.crt" \
+	EJBCA_UNTRUSTED="creds/PPKI_Playground_InfrastructureIssuingCAv10.crt" \
+	EJBCA_CMP_RECIPIENT="/CN=PPKI Playground ECC Issuing CA v1.0/OU=Corporate Technology/OU=For internal test purposes only/O=Siemens/C=DE" \
+	EJBCA_CMP_SERVER="/CN=Product PKI Playground CMP Signer/OU=PPKI Playground/OU=Corporate Technology/OU=For internal test purposes only/O=Siemens/C=DE" \
+	EJBCA_CMP_SUBJECT="/CN=test-genCMPClientDemo/OU=PPKI Playground/OU=Corporate Technology/OU=For internal test purposes only/O=Siemens/C=DE" \
+	EJBCA_CMP_SUBJECT_ECC="/CN=ECC-EE/OU=PPKI Playground/OU=Corporate Technology/OU=For internal test purposes only/O=Siemens/C=DE" \
+	EJBCA_ENABLED=1
+
 DID=${1:-did:example:123456789abcdefghi}
 KEY=${2:-creds/did.pem}
 CERT=${3:-creds/did.crt}

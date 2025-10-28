39c5673837ccea2c84b4c67956e8bbd5a2c03aa0
Dr. David von Oheimb <dev@ddvo.net>
2025-10-28 18:32:58 +0100

    credential_loading.c: make inclusion of storage/files_icv.h conditional

--- src/credential_loading.c
+++ src/credential_loading.c
@@ -20,10 +20,12 @@
 #include <secutils/credentials/credentials.h>
 #include <secutils/credentials/store.h>
 #include <secutils/credentials/cert.h>
+#ifdef SECUTILS_USE_ICV
+# include <secutils/storage/files_icv.h> /* for FILES_check_icv */
+#endif
 #include <secutils/connections/conn.h> /* for CONN_IS_HTTP[S] */
 #include <secutils/certstatus/crls.h> /* for CONN_load_crl_http */
 #include <secutils/util/log.h>
-#include <secutils/storage/files_icv.h>
 #include <genericCMPClient.h> /* for CRLs_free() */
 
 #include "credential_loading.h"

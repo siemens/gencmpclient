f746013f5e1e619f6442fa47f7bc803fd52b79e9
Quirin Gylstorff <quirin.gylstorff@siemens.com>
2025-10-28 09:23:25 +0100

    Fix implicit declaration of FILES_check_icv
    This was found during building for Debian 13(trixie).
    
    /build/reproducible-path/libgencmp-2.0/src/credential_loading.c: In function ‘STORE_load_more_check_ex’:
    /build/reproducible-path/libgencmp-2.0/src/credential_loading.c:902:12: error: implicit declaration of function ‘FILES_check_icv’ [-Wimplicit-function-declaration]
      902 |         || FILES_check_icv(ctx, file)
          |            ^~~~~~~~~~~~~~~
    At top level:

--- src/credential_loading.c
+++ src/credential_loading.c
@@ -23,6 +23,7 @@
 #include <secutils/connections/conn.h> /* for CONN_IS_HTTP[S] */
 #include <secutils/certstatus/crls.h> /* for CONN_load_crl_http */
 #include <secutils/util/log.h>
+#include <secutils/storage/files_icv.h>
 #include <genericCMPClient.h> /* for CRLs_free() */
 
 #include "credential_loading.h"

90fa26df8b1cdebf8e8e122bc43be7cac2c5f40b
Rajeev Ranjan <ranjan.rajeev@siemens.com>
2026-02-03 13:54:43 +0100

    fixup! add setup-mock.sh

--- test/recipes/80-test_cmp_http_data/Mock/setup-mock.sh
+++ test/recipes/80-test_cmp_http_data/Mock/setup-mock.sh
@@ -4,39 +4,50 @@ set -e
 
 # This script generates the certificates needed for the CMP server and the signer
 
-if [[ ! -f $mkcert_sh ]]; then
-    mkcert_sh="../../../certs/mkcert.sh"
+mkcert_sh="../../../certs/mkcert.sh"
+
+# Specify whether PQC or Classic algorithms are used
+# Possible values: "PQC" or "Classic"
+# Check if Algo_used is defined from environment variable, if not assign it "Classic"
+if [[ -z "${Algo_used}" ]]; then
+    Algo_used="Classic"
 fi
 
-# sever certificate algorithms
-if [ -z "$server_rootca_keyalg" ]; then
-    server_rootca_keyalg="MLDSA65"
-fi
-if [ -z "$server_leaf_keyalg" ]; then
-    server_leaf_keyalg="SLH-DSA-SHAKE-192s"
-fi
-# rootCACert update test case
-if [ -z "$new_rootca_keyalg" ]; then
-    new_rootca_keyalg="SLH-DSA-SHAKE-192s"
+if [ -z "$DAYS" ]; then  
+    DAYS=36524 # 100 years, with 24 leap years per 100 years until 2400  
 fi
 
-# End-entity certificate algorithms
-if [ -z "$signer_rootca_keyalg" ]; then
+if [[ ${Algo_used} == "PQC" ]]; then
+    # sever certificate algorithms
+    server_rootca_keyalg="SLH-DSA-SHAKE-192s"
+    server_leaf_keyalg="MLDSA65"
+    # rootCACert update test case
+    new_rootca_keyalg="MLDSA65"
+
+    # End-entity certificate algorithms
     signer_rootca_keyalg="MLDSA65"
-fi
-if [ -z "$signer_interca_keyalg" ]; then
     signer_interca_keyalg="MLDSA65"
-fi
-if [ -z "$signer_subinterca_keyalg" ]; then
     signer_subinterca_keyalg="MLDSA65"
-fi
-if [ -z "$signer_leaf_keyalg" ]; then
     signer_leaf_keyalg="SLH-DSA-SHAKE-192s"
+else
+    # sever certificate algorithms
+    server_rootca_keyalg="rsa"
+    server_leaf_keyalg="rsa"
+    # rootCACert update test case
+    new_rootca_keyalg="rsa"
+
+    # End-entity certificate algorithms
+    signer_rootca_keyalg="rsa"
+    signer_interca_keyalg="rsa"
+    signer_subinterca_keyalg="rsa"
+    signer_leaf_keyalg="rsa"
 fi
+
 
 # CMP server certificate
 rename_serverfiles() {
     echo "Renaming server files"
+    # removing unneeded files
     rm server_root-key.pem server_root-pubkey.pem newWithNew-key.pem newWithNew-pubkey.pem
     mv server_root-cert.pem server_root.crt
     cp server_root.crt trusted.crt
@@ -51,12 +62,13 @@ rename_serverfiles() {
 
 remove_serverfiles() {
     echo "Removing server files"
-    rm -f server.key server-crt trusted.crt server_root.crt
+    rm -f server_root.crt trusted.crt server.key server-crt
     rm -f oldWithOld.pem newWithNew.pem oldWithNew.pem newWithOld.pem
 }
 
-gen_servercert() {
+gen_server_chain() {
     remove_serverfiles
+    # allow time to sync file system after deletions
     sleep 5
     OPENSSL_KEYALG=${server_rootca_keyalg} \
     $mkcert_sh genroot "Root CA" server_root-key server_root-cert
@@ -68,13 +80,13 @@ gen_servercert() {
 
     openssl pkey -in newWithNew-key.pem -out newWithNew-pubkey.pem -outform PEM -pubout
     openssl x509 -new -subj "/CN=Root CA" -CA server_root-cert.pem -CAkey server_root-key.pem \
-        -out newWithOld.pem -force_pubkey newWithNew-pubkey.pem -extensions SAN \
-        -extfile <(printf "[SAN]\nbasicConstraints=critical,CA:true")
+        -out newWithOld.pem -force_pubkey newWithNew-pubkey.pem \
+        -extfile <(printf "basicConstraints=critical,CA:true")
 
     openssl pkey -in server_root-key.pem -out server_root-pubkey.pem -outform PEM -pubout
     openssl x509 -new -subj "/CN=Root CA" -CA newWithNew-cert.pem -CAkey newWithNew-key.pem \
-        -out oldWithNew.pem -force_pubkey server_root-pubkey.pem -extensions SAN \
-        -extfile <(printf "[SAN]\nbasicConstraints=critical,CA:true")
+        -out oldWithNew.pem -force_pubkey server_root-pubkey.pem \
+        -extfile <(printf "basicConstraints=critical,CA:true")
 
     rename_serverfiles
 }
@@ -97,8 +109,8 @@ rename_signerfiles() {
     mv signer_subinterCA-cert.pem issuing.crt
     mv signer_subinterCA-crl.pem newcrl.pem
     mv signer_leaf-key.pem new.key
-    openssl pkey -in new.key -out new_pass_12345.key -aes256 -passout pass:12345
-    echo "12345" > 12345.txt
+    openssl pkey -in new.key -out new_pass_12345.key -aes256 -passout file:12345.txt
+
     cp new.key signer.key
     mv signer_leaf-cert.pem signer_only.crt
     mv signer_issuing-cert.pem signer_issuing.crt
@@ -107,20 +119,28 @@ rename_signerfiles() {
 remove_signerfiles() {
     echo "Removing signer files"
     rm -f root.crt signer_root.crt newcrl.pem new.key signer.key signer_only.crt \
-        signer_issuing.crt signer.crt
+        signer_issuing.crt signer.crt issuing.crt
 }
 
+#  cannot use genee() because this uses a self-signature for the POP in a PKCS#10 CSR
 genee_kem() {
+    local cn=$1; shift
+    local key=$1; shift
+    local cert=$1; shift
+    local cakey=$1; shift
+    local ca=$1; shift
     echo "Generating KEM certificate"
-    openssl genpkey -algorithm "$OPENSSL_KEYALG" -out signer_leaf-key.pem -outpubkey signer_leaf-pubkey.pem
-    openssl x509 -new -subj "/CN=signer-leaf" -CA signer_subinterCA-cert.pem -CAkey signer_subinterCA-key.pem \
-        -out signer_leaf-cert.pem -force_pubkey signer_leaf-pubkey.pem -extensions SAN \
-        -extfile <(printf "[SAN]\nbasicConstraints=critical,CA:false\nkeyUsage=critical,keyEncipherment")
+    openssl genpkey -algorithm "$OPENSSL_KEYALG" -out ${key}.pem -outpubkey ${cn}-pubkey.pem
+    openssl x509 -new -subj "/CN=${cn}" -CA ${ca}.pem -CAkey ${cakey}.pem \
+        -out ${cert}.pem -force_pubkey ${cn}-pubkey.pem \
+        -extfile <(printf "basicConstraints=critical,CA:false\nkeyUsage=critical,keyEncipherment")
+    rm -f ${cn}-pubkey.pem
 }
 
-gen_signercert() {
+gen_client_chain() {
     echo "Generating signer certificates"
     remove_signerfiles
+    # allow time to sync file system after deletions
     sleep 5
     OPENSSL_KEYALG=${signer_rootca_keyalg} \
     $mkcert_sh genroot "signer-rootCA" signer_root-key signer_root-cert
@@ -130,20 +150,20 @@ gen_signercert() {
     $mkcert_sh genca "signer-subinterCA" signer_subinterCA-key signer_subinterCA-cert signer_interCA-key signer_interCA-cert
 
     OPENSSL_KEYALG=${signer_leaf_keyalg}
-    if [[ "$signer_leaf_keyalg" == *"MLKEM"* ]]; then
-        OPENSSL_KEYALG=${signer_leaf_keyalg} genee_kem
+    if [[ "$signer_leaf_keyalg" == *"KEM"* ]]; then
+        OPENSSL_KEYALG=${signer_leaf_keyalg} genee_kem "signer-leaf" signer_leaf-key signer_leaf-cert signer_subinterCA-key signer_subinterCA-cert
     else
         OPENSSL_KEYALG=${signer_leaf_keyalg} \
         $mkcert_sh genee -p clientAuth "signer-leaf" signer_leaf-key signer_leaf-cert signer_subinterCA-key signer_subinterCA-cert
     fi
 
     gen_demoCAfolder
-    openssl ca -gencrl -keyfile signer_subinterCA-key.pem -cert signer_subinterCA-cert.pem -out signer_subinterCA-crl.pem -crldays 36525 \
+    openssl ca -gencrl -keyfile signer_subinterCA-key.pem -cert signer_subinterCA-cert.pem -out signer_subinterCA-crl.pem -crldays $DAYS \
             -config <(printf "[ca]\ndefault_ca= CA_default\n[CA_default]\n%s\n%s\n%s\n" \
 		      "database = ./demoCA/index.txt" "crlnumber = ./demoCA/crlnumber" "default_md = default")
     cat signer_leaf-cert.pem signer_subinterCA-cert.pem signer_interCA-cert.pem > signer_chain.pem
     cat signer_subinterCA-cert.pem signer_interCA-cert.pem signer_root-cert.pem > signer_fullchain.pem
-    openssl pkcs12 -export -out signer.p12 -inkey signer_leaf-key.pem -in signer_leaf-cert.pem -certfile signer_fullchain.pem -password pass:12345
+    openssl pkcs12 -export -out signer.p12 -inkey signer_leaf-key.pem -in signer_leaf-cert.pem -certfile signer_fullchain.pem -password file:12345.txt
     rm -f signer_fullchain.pem
     cat signer_subinterCA-cert.pem signer_interCA-cert.pem > signer_issuing-cert.pem
     rename_signerfiles
@@ -152,8 +172,8 @@ gen_signercert() {
 
 
 all() {
-    gen_servercert
-    gen_signercert
+    gen_server_chain
+    gen_client_chain
 }
 
 "$@"

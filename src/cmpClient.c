ad4e3c4e3630bd873208384a714d34098292342c
Andreas Kretschmer <andreas.kretschmer@siemens.com>
2026-05-21 08:24:19 +0200

    fix parameter handling

--- src/cmpClient.c
+++ src/cmpClient.c
@@ -301,7 +301,7 @@ opt_t cmp_opts[] = {
       "Path to the RATS token configuration file for TPM key data request"},
     { "tpmkd_plugincfgpath", OPT_TXT, {.txt = NULL}, {(const char**) &opt_tpm_kd_req.plugconf_path },
       "Path to the RATS plugin configuration file for TPM key data request"},
-    { "tpmkd_plugincfgpath", OPT_TXT, {.txt = NULL}, {(const char**) &opt_tpm_kd_req.nonce },
+    { "tpmkd_nonce", OPT_TXT, {.txt = NULL}, {(const char**) &opt_tpm_kd_req.nonce },
       "Nonce for TPM key data request"},
 
     { "atcha_tokenname", OPT_TXT, {.txt = NULL}, {(const char**) &opt_attest_chal.token_name },
@@ -310,7 +310,7 @@ opt_t cmp_opts[] = {
       "Path to the RATS token configuration file for attestation challenge"},
     { "atcha_plugincfgpath", OPT_TXT, {.txt = NULL}, {(const char**) &opt_attest_chal.plugconf_path },
       "Path to the RATS plugin configuration file for attestation challenge"},
-    { "atcha_plugincfgpath", OPT_TXT, {.txt = NULL}, {(const char**) &opt_attest_chal.nonce },
+    { "atcha_nonce", OPT_TXT, {.txt = NULL}, {(const char**) &opt_attest_chal.nonce },
       "Nonce for attestation challenge"},
 
 
@@ -1240,16 +1240,15 @@ static int setup_ctx(CMP_CTX *ctx)
                 opt_tpm_kd_req.config_path == NULL ||
                 opt_tpm_kd_req.plugconf_path == NULL ||
                 opt_tpm_kd_req.nonce == NULL ||
-                opt_tpm_kd_req.token_name == NULL ||
 
                 opt_attest_chal.token_name == NULL ||
                 opt_attest_chal.config_path == NULL ||
                 opt_attest_chal.plugconf_path == NULL ||
                 opt_attest_chal.nonce == NULL
-                )
-            LOG_err("Incomplete RATS configuration");
-                       goto err;
-
+                ) {
+                LOG_err("Incomplete RATS configuration");
+                goto err;
+        }
         ctx->do_rats = true;
         ctx->tpm_kd_req = opt_tpm_kd_req;
         ctx->tpm_kd_req.nonce_size=strlen((char*)ctx->tpm_kd_req.nonce);
